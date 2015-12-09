34a5924a618d6d85ff578188e055c21643b47312
Teoman Soygul <tsoygul@gmail.com>
2015-12-09 17:30:28 +0100

    move todo items to github issues

--- app.py
+++ app.py
@@ -37,12 +37,7 @@ time.sleep(2)
 connect_button = driver.find_element_by_css_selector('#signInButton')
 connect_button.click()
 
-# wait for sign in
-time.sleep(15)
-
-# todo:
-# maybe we should listen for polymer events rather than waits
-# if spectron (https://github.com/kevinsawicki/spectron) is doing it, we can make a python port of it
+time.sleep(15)  # wait for sign in
 
 menu_button = driver.find_element_by_css_selector('#menuButton')
 menu_button.click()
