d74e199603d62a0976d4f61aeec8a09ce5bf1fe3
Teoman Soygul <tsoygul@gmail.com>
2015-12-09 17:14:20 +0100

    add basic test before page objects

--- app.py
+++ app.py
@@ -13,6 +13,7 @@
 # time.sleep(1)  # Let the user actually see something!
 # driver.quit()
 
+import os
 import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -24,9 +25,34 @@ opts = Options()
 opts.binary_location = electron_path
 driver = webdriver.Chrome(executable_path=chromedriver_path, chrome_options=opts)
 
-time.sleep(4)  # Let the user actually see something!
-search_box = driver.find_element_by_name('q')
-search_box.send_keys('ChromeDriver')
-search_box.submit()
-time.sleep(4)  # Let the user actually see something!
+time.sleep(3)
+
+username = driver.find_element_by_css_selector('#cards > div.cards.style-scope.task-cards > task-card > sign-in > div > div > paper-input:nth-child(3) > paper-input-container > div.input-content.style-scope.paper-input-container #labelAndInputContainer #input')
+username.send_keys('teoman.soygul')
+password = driver.find_element_by_css_selector('#cards > div.cards.style-scope.task-cards > task-card > sign-in > div > div > paper-input:nth-child(4) > paper-input-container > div.input-content.style-scope.paper-input-container #labelAndInputContainer #input')
+password.send_keys(os.environ['PASS'])
+
+time.sleep(2)
+
+connect_button = driver.find_element_by_css_selector('#signInButton')
+connect_button.click()
+
+time.sleep(15)  # wait for sign in (how to do this properly though?)
+
+menu_button = driver.find_element_by_css_selector('#menuButton')
+menu_button.click()
+time.sleep(1)
+logout_button = driver.find_element_by_css_selector('#contentWrapper > paper-material > paper-menu > div > a:nth-child(3)')
+logout_button.click()
+
+time.sleep(3)
+
+menu_button = driver.find_element_by_css_selector('#menuButton')
+menu_button.click()
+time.sleep(1)
+quit_button = driver.find_element_by_css_selector('#contentWrapper > paper-material > paper-menu > div > a:nth-child(4)')
+quit_button.click()
+
+time.sleep(2)
+
 driver.quit()
