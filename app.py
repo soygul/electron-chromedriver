9fec4649929fd246d66e68b21a2d279d1599c2c1
Teoman Soygul <tsoygul@gmail.com>
2015-12-10 13:50:20 +0100

    move optional code into github issues

--- app.py
+++ app.py
@@ -1,20 +1,3 @@
-# https://sites.google.com/a/chromium.org/chromedriver/getting-started
-
-# alternative for not restarting the chromedriver for every test case and continuously use existing instance
-
-# import time
-# from selenium import webdriver
-# import selenium.webdriver.chrome.service as service
-#
-# service = service.Service('./chromedriver')
-# service.start()
-# capabilities = {
-#     'chrome.binary': '/Users/teo/dev/sc-ui/dist/AppGate XDP-darwin-x64/AppGate XDP.app/Contents/MacOS/Electron'}
-# driver = webdriver.Remote(service.service_url, capabilities)
-# driver.get('http://www.google.com/xhtml')
-# time.sleep(1)  # Let the user actually see something!
-# driver.quit()
-
 import os
 import time
 from selenium import webdriver
