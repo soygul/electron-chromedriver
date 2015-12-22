4bf49a5714a8759763c3abd246921c6736107104
Teoman Soygul <tsoygul@gmail.com>
2015-12-22 16:45:42 +0100

    move menu part to page objects

---
+++ pageobjects/menu.py
@@ -0,0 +1,26 @@
+import time
+
+from pageobjects.base import PageObject
+
+
+class MainMenu(PageObject):
+    def __menu_button(self): return self.driver.find_element_by_css_selector('#menuButton')
+
+    def __logout_button(self): return self.driver.find_element_by_css_selector(
+            '#contentWrapper > paper-material > paper-menu > div > a:nth-child(3)')
+
+    def __quit_button(self): return self.driver.find_element_by_css_selector(
+            '#contentWrapper > paper-material > paper-menu > div > a:nth-child(4)')
+
+    def open_menu(self):
+        self.__menu_button().click()
+        time.sleep(1)  # slide-in animation time
+
+    def logout(self):
+        """Only available if user is already logged in."""
+        self.open_menu()
+        self.__logout_button().click()
+
+    def quit(self):
+        self.open_menu()
+        self.__quit_button().click()
--- run.py
+++ run.py
@@ -4,6 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 from pageobjects.login import LoginPage
+from pageobjects.menu import MainMenu
 
 chromedriver_path = "./chromedriver"
 electron_path = "/Users/teo/dev/sc-ui/dist/AppGate XDP-darwin-x64/AppGate XDP.app/Contents/MacOS/Electron"
@@ -13,29 +14,18 @@ opts.binary_location = electron_path
 driver = webdriver.Chrome(executable_path=chromedriver_path, chrome_options=opts)
 driver.implicitly_wait(15)  # seconds
 
-time.sleep(3)
+time.sleep(3)  # wait for application to start
 
 login_page = LoginPage(driver)
 login_page.login('teoman.soygul', os.environ['PASS'])
 
 time.sleep(15)  # wait for sign in
 
-menu_button = driver.find_element_by_css_selector('#menuButton')
-menu_button.click()
-time.sleep(1)
-logout_button = driver.find_element_by_css_selector(
-    '#contentWrapper > paper-material > paper-menu > div > a:nth-child(3)')
-logout_button.click()
+main_menu = MainMenu(driver)
+main_menu.logout()
 
-time.sleep(3)
+time.sleep(3)  # wait for logout
 
-menu_button = driver.find_element_by_css_selector('#menuButton')
-menu_button.click()
-time.sleep(1)
-quit_button = driver.find_element_by_css_selector(
-    '#contentWrapper > paper-material > paper-menu > div > a:nth-child(4)')
-quit_button.click()
-
-time.sleep(2)
+main_menu.quit()
 
 driver.quit()
