39218629b2d5e6b8df1fdcb46215f8c2818ab575
Teoman Soygul <tsoygul@gmail.com>
2015-12-22 12:24:02 +0100

    cleanup login page

--- pageobjects/login.py
+++ pageobjects/login.py
@@ -1,16 +1,9 @@
-from pageobjects.page import PageObject
+from pageobjects.base import PageObject
 
 
 class LoginPage(PageObject):
-    to = lambda self: self.webdriver.find_element_by_css_selector("#send-presentation-overlay .to-textarea")
-    subject = lambda self: self.webdriver.find_element_by_xpath("//*[@id='send-presentation-overlay']//input[@name='subject']")
-    message_text = lambda self: self.webdriver.find_element_by_css_selector("#send-presentation-overlay .message-textarea")
-    send_button = lambda self: self.webdriver.find_element_by_css_selector("#send-presentation-overlay .submit")
+    def __to(self): return self.driver.find_element_by_css_selector("#send-presentation-overlay .to-textarea")
 
 
-def send_message(self, to_address, subject, message):
+def send_message(self, to_address):
     self.to().send_keys(to_address)
-    self.subject().send_keys(subject)
-
-    # switch message input to text input to make it easier to set the message.
-    self.message_text().send_keys(message)
