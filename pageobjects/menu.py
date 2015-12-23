fbcd7e12f84ad0621092c6ee960b0ebe6b46dac5
Teoman Soygul <tsoygul@gmail.com>
2015-12-23 12:43:04 +0100

    __open_menu is now private

--- pageobjects/menu.py
+++ pageobjects/menu.py
@@ -12,15 +12,15 @@ class MainMenu(PageObject):
     def __quit_button(self): return self.driver.find_element_by_css_selector(
             '#contentWrapper > paper-material > paper-menu > div > a:nth-child(4)')
 
-    def open_menu(self):
+    def __open_menu(self):
         self.__menu_button().click()
         time.sleep(1)  # slide-in animation time
 
     def logout(self):
         """Only available if user is already logged in."""
-        self.open_menu()
+        self.__open_menu()
         self.__logout_button().click()
 
     def quit(self):
-        self.open_menu()
+        self.__open_menu()
         self.__quit_button().click()
